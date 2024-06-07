cd7e74a9ccb89759a4f47a703af98f97cd632076
sharjeel <sharjeelkhatri303@gmail.com>
2024-06-07 14:28:08 +0500

    the seconds are contineusly running

--- app.js
+++ app.js
@@ -1,47 +1,70 @@
+
+
 let date = document.getElementById("date");
 let days = document.getElementById("day");
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("mins");
+let leftsec = document.getElementById("sec")
+
+let today;
+let nextbirthday;
+
 
 function calnextBirthday() {
   
-  if (date.value === "") {
+  let input = date.value
+  // console.log(input);
+
+
+  if (input === "") {
     days.innerHTML = "Please enter a valid date";
     hours.innerHTML = "";
     minutes.innerHTML = "";
     return;
   }
   
-  let today = new Date();
-  let birthdate = new Date(date.value);
-  let nextbirthday;
+  today = new Date();
+  let birthdate = new Date(input);
+  nextbirthday = new Date(today.getFullYear(), birthdate.getMonth(), birthdate.getDate())
 
-  if (
-    birthdate.getMonth() < today.getMonth() ||
-    (birthdate.getMonth() === today.getMonth() &&
-      birthdate.getDate() < today.getDate())
-  ) {
-    nextbirthday = new Date(
-      today.getFullYear() + 1,
-      birthdate.getMonth(),
-      birthdate.getDate()
-    );
-  } else {
-    nextbirthday = new Date(
-      today.getFullYear(),
-      birthdate.getMonth(),
-      birthdate.getDate()
-    );
+  
+  if(nextbirthday < today){
+    nextbirthday.setFullYear(today.getFullYear() + 1)
   }
-
-  let difference = nextbirthday.getTime() - today.getTime();
-  let leftDays = Math.ceil(difference / (1000 * 3600 * 24));
-  let leftHours = Math.ceil(difference / (1000 * 3600));
-  let leftmins = Math.ceil(difference / (1000 * 60));
-
-  // console.log(leftDays);
-
-  days.innerHTML = "Your next birthday is in " + leftDays + " days";
-  hours.innerHTML = "And in " + leftHours + " hours";
-  minutes.innerHTML = "And in " + leftmins + " minutes";
+  // console.log("Next birthday:", nextbirthday);  // Debug log
+// 
+  update()
+  
 }
+  function update(){
+    if(!nextbirthday){
+      return
+    }
+
+today = new Date()
+let difference = nextbirthday.getTime() - today.getTime();
+let leftDays = Math.ceil(difference / (1000 * 3600 * 24));
+let leftHours = Math.ceil(difference / (1000 * 3600));
+let leftmins = Math.ceil(difference / (1000 * 60));
+let leftsec = Math.ceil(difference / 1000)
+
+// console.log(leftDays);
+
+
+days.innerHTML = "Your next birthday is in " + leftDays + " days";
+hours.innerHTML = "And in " + leftHours + " hours";
+minutes.innerHTML = "And in " + leftmins + " minutes";
+sec.innerHTML = "And in " + leftsec + "seconds"
+}
+
+
+  document.querySelector("form").addEventListener("submit" , (event) =>{
+    event.preventDefault()
+    calnextBirthday()
+  })
+
+  // date.addEventListener("change", calnextBirthday)
+
+
+setInterval(update, 1000)
+
